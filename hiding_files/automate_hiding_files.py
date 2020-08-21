ea64b5c5fc5487b8f6c9486f13d4fb89b1b6e99b
Sonu Kumar Gautam <sonukumargautam41638@gmail.com>
2020-08-21 14:35:07 +0530

    getting back the hidden files

--- hiding_files/automate_hiding_files.py
+++ hiding_files/automate_hiding_files.py
@@ -18,7 +18,7 @@ if operation=='1':
     saved_paths=[]
     for dirpath,dirnames,filenames in os.walk(working_directory):
         for filename in filenames:
-            if filename[ :len(key_name)]==key_name and str(os.path.splitext(filename)[1]) in ('.mp4','png','jpg','jpeg'):
+            if filename[ :len(key_name)]==key_name and str(os.path.splitext(filename)[1]) in ('.mp4','.png','.jpg','.jpeg'):
                 if dirpath!=secret_directory:
                     original={
                         'file_name':filename,
@@ -29,11 +29,14 @@ if operation=='1':
                 if len(saved_paths)>0:
                     with open(f"{secret_directory}/secret.json",'w') as file:
                         json.dump(saved_paths,file,indent=2)
+    print('Files Hiding Successful!!')
 elif operation=='2':
     with open(f"{secret_directory}/secret.json",'r') as file:
         python_data=json.load(file)
     for file_name in os.listdir(secret_directory):
         for file_dict in python_data:
             if file_name==file_dict['file_name']:
-                os.system(f'mv \"{str(os.path.join(f"{secret_directory}/" + str(file_name)))}\"' f'\"{str(os.path.join(file_dict["dir_path"]))}/\"')
-
+                os.system(f'mv \"{str(os.path.join(f"{secret_directory}/" + str(file_name)))}\" '
+                          f'\"{str(os.path.join(file_dict["dir_path"]))}/\"')
+    print("Got Back the Hidden File!!")
+    
